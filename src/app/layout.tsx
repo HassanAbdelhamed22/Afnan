e38dfbf19ce65536ee54d13717c62cdfee84c829
HassanAbdelhamed22 <104987743+HassanAbdelhamed22@users.noreply.github.com>
2026-08-04 07:14:52 +0300

    fix: add suppressHydrationWarning to avoid The hydration mismatch error is a very common issue caused by browser extensions

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -25,6 +25,7 @@ export default function RootLayout({ children }: LayoutProps<"/">) {
     <html
       lang="en"
       className={`${ebGaramond.variable} ${manrope.variable} h-full antialiased`}
+      suppressHydrationWarning
     >
       <body className="min-h-full flex flex-col">{children}</body>
     </html>
